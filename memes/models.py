295b5de2fb799a479491aca07f22c603167677e1
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2019-03-27 20:26:14 -0400

    First Commit

---
+++ memes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Meme(models.Model):
+	name = models.CharField(max_length=50)
+	created_date = models.DateTimeField()
+	img_url = models.URLField(max_length=200)
+	description = models.TextField()
+	upvote_peak = models.IntegerField(default=0)
+
