7292228a3fe02db855d0db45413f80e9546e02e5
earbeitman <evan.arbeitman@compass.com>
2019-04-16 22:50:49 -0400

    remove redunant route to memes api

--- PikaPapow/urls.py
+++ PikaPapow/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
-	path('api/memes/', include('memes.urls')),
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('memes.urls'))
 
